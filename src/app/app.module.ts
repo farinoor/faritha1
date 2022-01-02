dfc62fd3b2ae1eac7549d50ae984ca514ac99288
farinoor <farithanoormohammed@gmail.com>
2022-01-02 10:59:16 +0530

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DummyComponent } from './dummy/dummy.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dummy', component: DummyComponent },
+  { path: 'employee', component: EmployeeComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, DummyComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    DummyComponent,
+    SignupComponent,
+    LoginComponent,
+    NavbarComponent,
+    EmployeeComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
