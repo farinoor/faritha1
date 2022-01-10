83a516359a8c80e98664b3cc63f2e195cb3d78b3
farinoor <farithanoormohammed@gmail.com>
2022-01-10 08:30:33 +0530

    3 files modified

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -6,20 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dummy.component.css'],
 })
 export class DummyComponent implements OnInit {
+  conditionApply = true;
   constructor() {}
 
   ngOnInit() {}
+  changing() {
+    if (this.conditionApply) this.conditionApply = false;
+    else this.conditionApply = true;
+  }
   hello() {
     // alert('hello');
     this.controlType = this.controlType == 'text' ? 'password' : 'text';
   }
   controlType = 'text';
   hello1 = 'hello';
-  
-    welcome()
-    {
-      alert('welcome');
-  
 
-    }
-   }
+  welcome() {
+    alert('welcome');
+  }
+}
