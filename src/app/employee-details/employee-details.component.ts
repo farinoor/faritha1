eb3e1d39d6cfda61b5ff32fba9e55faa6c2b1307
farinoor <farithanoormohammed@gmail.com>
2022-01-23 21:04:03 +0530

    3 files modified, 1 added

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.css']
+  styleUrls: ['./employee-details.component.css'],
 })
 export class EmployeeDetailsComponent implements OnInit {
+  employeeDetails = [
+    {
+      name: 'Faritha',
+      email: 'farithanoormohammed@gmail.com',
+      mobile: '123456789',
+    },
+  ];
+  constructor() {}
+  back() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -25,6 +25,7 @@ export class EmployeeComponent implements OnInit {
   ];
 
   constructor() {}
+  viewDetails() {}
 
   ngOnInit() {}
 }
