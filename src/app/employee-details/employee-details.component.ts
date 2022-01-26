5f93935dc19c9f98a3db82729dffde528bfc3ee0
farinoor <farithanoormohammed@gmail.com>
2022-01-26 09:39:27 +0530

    4 files modified

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -13,6 +13,35 @@ export class EmployeeDetailsComponent implements OnInit {
       mobile: '123456789',
     },
   ];
+  employee = {
+    Name: 'Aurora',
+    Age: 53,
+    Email: 'ante.blandit@disparturient.ca',
+    'Date of joining': '10/06/2019',
+    'Phone number': '1628101902299',
+    Street: '493 Iaculis Rd.',
+    City: 'Lewiston',
+    Zip: '42591-180',
+    Region: 'Maine',
+    Country: 'Sudan',
+    Info: 'est. Nunc ullamcorper, velit in aliquet lobortis, nisi nibh lacinia',
+    EmployeeID: 101,
+  };
+  employee2 = {
+    Name: 'Lamar',
+    Age: 64,
+    Email: 'Quisque.tincidunt@SuspendisseduiFusce.com',
+    'Date of joining': '18/10/2017',
+    'Phone number': '1665061376299',
+    Street: '510 Ante. Av.',
+    City: 'Skegness',
+    Zip: '4220',
+    Region: 'Lincolnshire',
+    Country: 'Samoa',
+    Info: 'mi tempor lorem, eget mollis lectus pede et risus. Quisque',
+    EmployeeID: 110,
+  };
+
   constructor() {}
   backDetails() {}
 
